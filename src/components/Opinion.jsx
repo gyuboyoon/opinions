66da01c69482540c804591b34973932c11d180be
gyubo <yoon9yubo@gmail.com>
2025-02-04 23:30:36 +0900

    <form> 제출 훅 추가

--- src/components/Opinion.jsx
+++ src/components/Opinion.jsx
@@ -1,11 +1,30 @@
+import { use, useActionState, useOptimistic } from "react";
+
+import { OpinionsContext } from "../store/opinions-context";
+
 export function Opinion({ opinion: { id, title, body, userName, votes } }) {
-  function upvoteAction() {
-    console.log("up");
+  const { upvoteOpinion, downvoteOpinion } = use(OpinionsContext);
+
+  const [optimisticVotes, setVotesOptimistically] = useOptimistic(
+    votes,
+    (prevVotes, mode) => (mode === "up" ? prevVotes + 1 : prevVotes - 1)
+  );
+  // 최종적으로 낙관적으로 업데이트 할 값, 리엑트에의해 자동으로 전달되는 값, 함수가 호출되기 전 활성 상태였거나, 호출될 때 여전히 활성 상태일 값-> 새로운 값을 반환하는 역할을 할것
+
+  async function upvoteAction() {
+    setVotesOptimistically("up");
+    await upvoteOpinion(id);
   }
 
-  function downvoteAction() {
-    console.log("down");
+  async function downvoteAction() {
+    setVotesOptimistically("down");
+    await downvoteOpinion(id);
   }
+
+  const [upvoteFormState, upvoteFormAction, upvotePending] =
+    useActionState(upvoteAction);
+  const [downvoteFormState, downvoteFormAction, downvotePending] =
+    useActionState(downvoteAction);
 
   return (
     <article>
@@ -15,7 +34,10 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
       </header>
       <p>{body}</p>
       <form className="votes">
-        <button formAction={upvoteAction}>
+        <button
+          formAction={upvoteFormAction}
+          disabled={upvotePending || downvotePending}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -33,9 +55,12 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
           </svg>
         </button>
 
-        <span>{votes}</span>
+        <span>{optimisticVotes}</span>
 
-        <button formAction={downvoteAction}>
+        <button
+          formAction={downvoteFormAction}
+          disabled={upvotePending || downvotePending}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
